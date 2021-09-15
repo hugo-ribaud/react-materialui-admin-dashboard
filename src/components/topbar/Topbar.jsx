4d0eeb1641aa5d162e5d7516829eda94f2b642c8
Hugo <hugo.ribaud@epitech.eu>
2021-09-15 16:23:40 +0200

    push

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./topbar.css";
+import Avatar from "../../assets/avatar.png"
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Dashboard Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src={Avatar} alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
